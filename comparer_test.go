4b25c684809fa5a57e2ad4187b712236396ad7d6
Matias Sanchez <13229873+kupuka@users.noreply.github.com>
2021-06-28 14:07:08 -0300

    #2, #3: Add test cases for pointers values

--- comparer_test.go
+++ comparer_test.go
@@ -30,46 +30,63 @@ var groups = map[string][]tc{
 		{[2]int{1, 2}, [2]int{3, 4}},
 		{[2]int{1, 2}, [2]int{2, 1}},
 		{[2]int{1}, [2]int{1, 2}},
+		{[2]int{1, 2}, [2]int{1, 2}},
 	},
 	"Bool": {
 		{nil, true},
 		{nil, false},
 		{true, false},
+		{true, true},
+		{false, false},
 	},
 	"Float32": {
 		{nil, float32(0.2)},
 		{float32(0.2), float32(1.7)},
 		{float32(-3.2), float32(3.2)},
+		{float32(0.2), float32(0.2)},
+		{float32(-3.2), float32(-3.2)},
 	},
 	"Float64": {
 		{nil, float64(0.2)},
 		{float64(0.2), float64(1.7)},
 		{float64(-3.2), float64(3.2)},
+		{float64(0.2), float64(0.2)},
+		{float64(-3.2), float64(-3.2)},
 	},
 	"Int": {
 		{nil, int(1)},
 		{int(1), int(2)},
 		{int(-1), int(1)},
+		{int(1), int(1)},
+		{int(-1), int(-1)},
 	},
 	"Int8": {
 		{nil, int8(1)},
 		{int8(1), int8(2)},
 		{int8(-1), int8(1)},
+		{int8(1), int8(1)},
+		{int8(-1), int8(-1)},
 	},
 	"Int16": {
 		{nil, int16(1)},
 		{int16(1), int16(2)},
 		{int16(-1), int16(1)},
+		{int16(1), int16(1)},
+		{int16(-1), int16(-1)},
 	},
 	"Int32": {
 		{nil, int32(1)},
 		{int32(1), int32(2)},
 		{int32(-1), int32(1)},
+		{int32(1), int32(1)},
+		{int32(-1), int32(-1)},
 	},
 	"Int64": {
 		{nil, int64(1)},
 		{int64(1), int64(2)},
 		{int64(-1), int64(1)},
+		{int64(1), int64(1)},
+		{int64(-1), int64(-1)},
 	},
 	"Map": {
 		{nil, map[string]int(nil)},
@@ -81,6 +98,7 @@ var groups = map[string][]tc{
 		{map[string]int{"A": 1, "B": 2}, map[string]int{"C": 1, "D": 2}},
 		{map[string]int{"A": 1, "B": 2}, map[string]int{"A": 3, "B": 4}},
 		{map[string]int{"A": 1, "B": 2}, map[string]int{"A": 1}},
+		{map[string]int{"A": 1, "B": 2}, map[string]int{"A": 1, "B": 2}},
 	},
 	"Slice": {
 		{nil, []int(nil)},
@@ -92,6 +110,7 @@ var groups = map[string][]tc{
 		{[]int{1, 2}, []int{3, 4}},
 		{[]int{1, 2}, []int{2, 1}},
 		{[]int{1}, []int{1, 2}},
+		{[]int{1, 2}, []int{1, 2}},
 	},
 	"String": {
 		{nil, "test1"},
@@ -99,38 +118,46 @@ var groups = map[string][]tc{
 		{"", "test1"},
 		{"test1", "test2"},
 		{"test1", "TEST1"},
+		{"test1", "test1"},
 	},
 	"Struct": {
 		{nil, es1{1, "A"}},
 		{es1{1, "A"}, es2{1, "A"}},
-		{es1{1, "A"}, es1{2, "B"}},
+		{es1{1, "A"}, es1{1, "B"}},
+		{es1{1, "A"}, es1{1, "A"}},
 	},
 	"Uint": {
 		{nil, uint(1)},
 		{uint(1), uint(2)},
+		{uint(1), uint(1)},
 	},
 	"Uint8": {
 		{nil, uint8(1)},
 		{uint8(1), uint8(2)},
+		{uint8(1), uint8(1)},
 	},
 	"Uint16": {
 		{nil, uint16(1)},
 		{uint16(1), uint16(2)},
+		{uint16(1), uint16(1)},
 	},
 	"Uint32": {
 		{nil, uint32(1)},
 		{uint32(1), uint32(2)},
+		{uint32(1), uint32(1)},
 	},
 	"Uint64": {
 		{nil, uint64(1)},
 		{uint64(1), uint64(2)},
+		{uint64(1), uint64(1)},
 	},
 }
 
-func TestEqual(t *testing.T) {
+func TestDefaultEqual(t *testing.T) {
 	c := comparer.Default()
 
-	run := func(t *testing.T, a interface{}, b interface{}, e bool) {
+	run := func(t *testing.T, a interface{}, b interface{}) {
+		e := reflect.DeepEqual(a, b)
 		if c.Equal(a, b) != e {
 			if e {
 				t.Errorf("The values should be equal")
@@ -143,14 +170,23 @@ func TestEqual(t *testing.T) {
 	for name, cases := range groups {
 		t.Run(name, func(t *testing.T) {
 			for _, tc := range cases {
-				t.Run(fmt.Sprintf("comparer.Equal(%+v, %+v)", tc.a, tc.a), func(t *testing.T) {
-					run(t, tc.a, tc.a, reflect.DeepEqual(tc.a, tc.a))
-				})
 				t.Run(fmt.Sprintf("comparer.Equal(%+v, %+v)", tc.a, tc.b), func(t *testing.T) {
-					run(t, tc.a, tc.b, reflect.DeepEqual(tc.a, tc.b))
+					run(t, tc.a, tc.b)
 				})
 				t.Run(fmt.Sprintf("comparer.Equal(%+v, %+v)", tc.b, tc.a), func(t *testing.T) {
-					run(t, tc.b, tc.a, reflect.DeepEqual(tc.b, tc.a))
+					run(t, tc.b, tc.a)
+				})
+				t.Run(fmt.Sprintf("comparer.Equal(&%+v, %+v)", tc.a, tc.b), func(t *testing.T) {
+					run(t, &tc.a, tc.b)
+				})
+				t.Run(fmt.Sprintf("comparer.Equal(%+v, &%+v)", tc.b, tc.a), func(t *testing.T) {
+					run(t, tc.b, &tc.a)
+				})
+				t.Run(fmt.Sprintf("comparer.Equal(&%+v, &%+v)", tc.a, tc.b), func(t *testing.T) {
+					run(t, &tc.a, &tc.b)
+				})
+				t.Run(fmt.Sprintf("comparer.Equal(&%+v, &%+v)", tc.b, tc.a), func(t *testing.T) {
+					run(t, &tc.b, &tc.a)
 				})
 			}
 		})
